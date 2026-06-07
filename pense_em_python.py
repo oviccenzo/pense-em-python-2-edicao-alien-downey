3519ab96b98d95cbc68c8316ccbf110bc88d045e
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-06-07 18:59:03 -0300

    Update pense_em_python.py

--- pense_em_python.py
+++ pense_em_python.py
@@ -190,7 +190,7 @@ n = 17 *22
 
 print(n)
 
-print(n + 25 ** 2)  
+print(n + 25 ** 2)
 
 n1 = 17
 print(n1)
@@ -244,7 +244,7 @@ minutes3 = (minuto3 * 100) / 60
 print(minutes3)
 
 minuto4 = 60
-minutes4 = (minuto4 * 100) / 60  
+minutes4 = (minuto4 * 100) / 60
 print(minutes4)
 
 
@@ -252,7 +252,7 @@ print(minutes4)
 ###exponenciação e a proxima precedencia que possui mais alta da quantidade de numero
 
 import math
-print(int(1 + math.pow(2, 2))) 
+print(int(1 + math.pow(2, 2)))
 print(int(2 + math.pow(2, 3)))
 print(int(3 + math.pow(2, 4)))
 print(int(4 + math.pow(2, 5)))
@@ -522,7 +522,7 @@ print(6+6/2)
 print(6+7/2)
 print(6+8/2)
 print(6+9/1)
-print(6+10/2) 
+print(6+10/2)
 
 ####Esse são exemplo do livro pense em python esse exemplo são multplicação e divisão que tem a alta precedencia do que a adição e da subtração
 
@@ -1366,8 +1366,10 @@ import math
 
 radians3 = float(input("Digite qualquer número: "))
 radius = float(input("Digite qualquer número: "))
+expoente = float(input("Digite qualquer número: "))
 
-e = math.exp(1.0)
+
+e = math.exp(expoente)
 height = radius * math.sin(radians3)
 
 print(f"O valor dor resultado do expoente de e eh: {e}")
@@ -1428,9 +1430,110 @@ def distancia1(x14,y14,x15,y15):
 
 distancia1(1,2,4,6)
 
+import math
+
+def distancia2(x16,y16,x17,y17):
+  dx = x16 - y17
+  dy = y16 - y17
+  dsquared = dx**2 + dy**2
+  print(f"dsquared is: {dsquared}")
+  return
+distancia2(1,2,4,5)
+
+import math
+
+def distancia3(x18,y18,x19,y19):
+  dx1 = x18 - y19
+  dy1 = y18 - y19
+  dsquared1 = dx1 ** 2 + dy1 ** 2
+  result = math.sqrt(dsquared1)
+  return result
+
+distancia3(1,2,4,6)
+
+##Composição
+
+####Calcular area do circulo e o ponto no perimetro
+
+import math
+
+def area2(radius6):
+  radius6 = float(input("Digite qualquer numero: "))
+  return math.pi * radius6 ** 2
+
+def circle_area(xc, yc, xp, yp):
+  radius6 = distancia(xc, yc, xp, yp)
+  result = area2(radius6)
+  return result 
+
+circle_area(1,2,4,6)
+
+import math
+def area3 (radius7):
+  return math.pi * radius7 ** 2
+
+def circle_area1(xc1,xy1,xp1,yp1):
+  radius7 = distancia1(xc1,xy1,xp1,yp1)
+
+
+##Funções booleanos
+
+####Retornando a funções booleanos
+
+def is_divisible(x19, y19):
+  if x19 % y19 == 0:
+    return True
+  else:
+    return False
+
+print(is_divisible(6,4))
+print(is_divisible(6,3))
+
+def is_divisible1(x20, y20):
+  return x20 % y20 == 0
+print(is_divisible1(6,4))
+print(is_divisible1(6,3))
+
+###Funções booleanos incondicionais
+
+def is_divisible2(x21,y21):
+  print("x is divisible by y")
+  return x21 % y21 == 0
+
+print(is_divisible2(6,4))
+print(is_divisible2(6,3))
+
+def is_divisible3(x22,y22):
+  if x22 % y22 == True:
+    print("x is divisible by y")
+
+def is_between(x,y,z):
+  return x <= y <= z
+
+print(is_between(8,9,11)) 
+
+##Mais recursividade
+
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    recurse = factorial(n-1)
+    result3 = n * recurse
+    return result3
+
+print(factorial(5))
+print(factorial(6))
+print(factorial(7))
+print(factorial(8))
+
 # Capitulo 7: Iteração
 
+x16 = 1
+x16
 
+x17 = 7
+x17
 
 # Capitulo 8: Strings
 
